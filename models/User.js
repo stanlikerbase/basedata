c0733b7e9db4604d97e33ff6e4d5c89fdc3b1cde
stanlikerbase <157885122+stanlikerbase@users.noreply.github.com>
2024-06-16 19:06:22 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,55 +1,60 @@
-import mongoose, { Schema } from 'mongoose'
+import mongoose, { Schema } from 'mongoose';
 
 const validateSettings = function (value) {
-	const settingsKeys = Object.keys(value)
-	return (
-		settingsKeys.length <= 5 &&
-		settingsKeys.every(key => typeof value[key] === 'object')
-	)
-}
+    const settingsKeys = Object.keys(value);
+    return (
+        settingsKeys.length <= 5 &&
+        settingsKeys.every(key => typeof value[key] === 'object')
+    );
+};
 
 const UserSchema = new mongoose.Schema(
-	{
-		fullName: {
-			type: String,
-			required: true,
-		},
-		email: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-		passwordHash: {
-			type: String,
-			required: true,
-		},
-		subscribe: {
-			type: String,
-			required: true,
-		},
-		avatarUrl: String,
-		settings: {
-			type: Schema.Types.Mixed,
-			default: {},
-			validate: [
-				{
-					validator: validateSettings,
-					message: 'Settings object can contain up to 5 key-value pairs.',
-				},
-			],
-		},
-		connections: {
-			type: Number,
-			default: 0,
-		},
-		maxConnections: {
-			type: Number,
-			default: 20,
-		},
-	},
-	{
-		timestamps: true,
-	}
-)
+    {
+        fullName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        passwordHash: {
+            type: String,
+            required: true,
+        },
+        subscribe: {
+            type: String,
+            required: true,
+        },
+        avatarUrl: String,
+        settings: {
+            type: Schema.Types.Mixed,
+            default: {},
+            validate: [
+                {
+                    validator: validateSettings,
+                    message: 'Settings object can contain up to 5 key-value pairs.',
+                },
+            ],
+        },
+        connections: {
+            type: Number,
+            default: 0,
+        },
+        maxConnections: {
+            type: Number,
+            default: 20,
+        },
+        telegramID: {
+            type: String,
+            unique: true,
+            sparse: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
 
-export default mongoose.model('User', UserSchema)
+export default mongoose.model('User', UserSchema);
