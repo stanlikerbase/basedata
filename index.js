ad27f30bd259dc89065d17e77b789dbc9cef1d7b
stanlikerbase <157885122+stanlikerbase@users.noreply.github.com>
2024-07-03 22:45:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,21 +20,16 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.post('/auth/login', loginValidation, UserController.login)
-app.post('/auth/change-pass', UserController.changePassword)
 app.get('/auth/logout', UserController.logout)
 app.get('/auth/me', checkAuth, UserController.getMe)
+app.get('/logins', UserController.getAllLogins)
+
+app.post('/auth/login', loginValidation, UserController.login)
+app.post('/auth/change-pass', UserController.changePassword)
 app.post('/auth/change-subscribe', UserController.changeSubscribe);
 app.post('/auth/addTelegram', UserController.saveTelegramID)
-app.get(
-	'/delete-all-sessions',
-	checkAuth,
-	UserController.deleteAllUserSessionsByEmail
-)
 app.post('/auth/register', registerValidation, UserController.register)
-
 app.post('/auth/user', UserController.getUser)
-app.get('/logins', UserController.getAllLogins)
 app.post('/save-settings', checkAuth, UserController.updateUserSetting)
 app.post('/get-settings', checkAuth, UserController.getUserSetting)
 app.post('/delete-settings', checkAuth, UserController.deleteUserSetting)
